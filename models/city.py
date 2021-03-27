f0e148a31c85702ca064db18e036eeeefcad5758
Themis <midinfotn401@gmail.com>
2021-03-27 23:05:09 +0100

    console have bugs and base model two

--- models/city.py
+++ models/city.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel,Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if models.storage_type == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        super().__init__(*args,**kwargs)
+
--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+
+class State(BaseModel,Base):
     """ State class """
-    name = ""
+    if models.storage_type == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self,*args,**kwargs):
+        """
+        """
+        super().__init__(*args,**kwargs)
