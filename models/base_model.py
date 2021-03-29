a4cb39022e6d073b5cfbb29db825cb78711f5b5f
Themis <midinfotn401@gmail.com>
2021-03-29 12:44:29 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel:
     """A base class for all hbnb models"""
     if models.storage_type == "db":
         id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-        updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """
