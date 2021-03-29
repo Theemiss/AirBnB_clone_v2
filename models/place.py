3c5ca1d5ccaa2d97a09bcb815f2d10d138123d37
Themis <midinfotn401@gmail.com>
2021-03-29 20:00:41 +0100

    fix

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ class Place(BaseModel):
     if models.storage_type == "db":
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey('user_id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
