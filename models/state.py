d592812416215e44609ee5f6e322e73d45f5ed9b
Themis <midinfotn401@gmail.com>
2021-03-29 19:26:21 +0100

    fix

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
+from models.city import City
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -21,3 +22,17 @@ class State(BaseModel, Base):
         init inherited
         """
         super().__init__(*args, **kwargs)
+
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """getter for cities that return
+            a list of city instance equale to
+            curent state id
+            """
+            list_city = []
+            all_inst_c = models.storage.all(City)
+            for value in all_inst_c.values():
+                if value.state_id == self.id:
+                    list_city.append(value)
+            return list_city
