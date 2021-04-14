680288d3b8cf9d7374cc93ecc58fb5617a7ac1f2
Themis <midinfotn401@gmail.com>
2021-04-14 16:27:57 +0100

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,12 +4,12 @@ sudo apt-get update -y
 sudo apt-get install -y nginx
 sudo chown -R "$USER":"$USER" /var/www/
 sudo chown -R "$USER":"$USER" /etc/nginx
-mkdir -R  /data/web_static/
-mkdir -R /data/web_static/releases/test/
+mkdir -p  /data/web_static/
+mkdir -p /data/web_static/releases/test/
 mkdir /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Hello AirBnb" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current 
-chown ubuntu:ubuntu -R /data/
-# ToDo: Config ngnix Using Alias
+chown ubuntu:ubuntu -hR /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart 
